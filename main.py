94ec556572a280b97cc1901983f15e803cb42754
Sergey Kan <sergeykan@MacBook-Air-Sergey.local>
2024-04-25 11:28:11 +0400

    completed

--- main.py
+++ main.py
@@ -13,6 +13,23 @@ STOCK_API_URL = os.getenv("STOCK_API_URL")
 NEWS_API_KEY = os.getenv("NEWS_API_KEY")
 NEWS_API_URL = os.getenv("NEWS_API_URL")
 
+
+def get_news():
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "q": COMPANY_NAME,
+        "from": yesterday
+    }
+    news_response = requests.get(NEWS_API_URL, news_params)
+    news_data = news_response.json()
+
+    articles_data = news_data["articles"]
+
+    message = f'Headline: {articles_data[2]["title"]}\n'
+    message += f'Brief: {articles_data[2]["description"]}'
+
+    return message
+
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 stock_params = {
@@ -32,28 +49,14 @@ daily_data = stock_data["Time Series (Daily)"]
 yesterday_close = float(daily_data[yesterday]["4. close"])
 day_before_yesterday_close = float(daily_data[day_before_yesterday]["4. close"])
 
-print(yesterday_close)
-print(day_before_yesterday_close)
-
 # check the difference (positive or negative)
 if abs(yesterday_close / day_before_yesterday_close - 1) > 0.05:
     is_positive = yesterday_close / day_before_yesterday_close > 1
     percentage = int(abs(yesterday_close / day_before_yesterday_close - 1)*100)
 
-    print("difference", percentage)
-
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
-news_params = {
-    "apiKey": NEWS_API_KEY,
-    "q": COMPANY_NAME,
-    "from": day_before_yesterday
-}
-news_response = requests.get(NEWS_API_URL, news_params)
-news_data = news_response.json()
-
-# for i in range(0, 3):
-
+    message = f'{"🔺" if is_positive else "🔻"}{percentage}%\n'
+    message += get_news()
+    print(message)
 
 ## STEP 3: Use https://www.twilio.com
 # Send a seperate message with the percentage change and each article's title and description to your phone number. 
